8ea88a419fc82093dc6589bf2e1af9482450f13e
Niklas Tinglöf <niklas.tinglof@devport.se>
2016-03-21 23:39:17 +0100

    -updated todos

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -603,11 +603,11 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
  * MOVEMENT SETTINGS
  */
 
-// todo: update HOMING_FEEDRATE
+// todo: makersnkpg: update HOMING_FEEDRATE
 #define HOMING_FEEDRATE {10*60, 10*60, 2*60, 0}  // set the homing speeds (mm/min)
 
 // default settings
-// todo: update...
+// todo: makersnkpg: update axis behaviour...
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {198.42,198.42,6400,101}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {40, 40, 2, 40}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {200,200,50,2000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
